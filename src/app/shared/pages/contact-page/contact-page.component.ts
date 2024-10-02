8be5bb05c4713d40e40be368ad091d77cac212df
C.Manuel Hernández <cmanuelhm87@gmail.com>
2024-10-02 09:17:36 +0200

    RouterLink y RouterLinkOptions

---
+++ src/app/shared/pages/contact-page/contact-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-page',
+  templateUrl: './contact-page.component.html',
+  styles: [],
+})
+export class ContactPageComponent {}
