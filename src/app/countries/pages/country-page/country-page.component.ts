a5704ccf016c7141e61251010397dac4ea58bb40
C.Manuel Hernández <cmanuelhm87@gmail.com>
2024-10-02 12:21:33 +0200

    Mostrar información del país

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CountrieService } from '../../services/countries.service';
 import { switchMap } from 'rxjs';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-country-page',
@@ -9,6 +10,8 @@ import { switchMap } from 'rxjs';
   styles: [],
 })
 export class CountryPageComponent implements OnInit {
+  public country?: Country;
+
   constructor(
     private activatedRoute: ActivatedRoute,
     private countrieService: CountrieService,
@@ -24,8 +27,7 @@ export class CountryPageComponent implements OnInit {
         if (!country) {
           return this.router.navigateByUrl('');
         }
-        console.log('Hay pais');
-        return;
+        return (this.country = country);
       });
   }
 }
